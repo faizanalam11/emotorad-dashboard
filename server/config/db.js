17e2f8adcc8e3cc2984555cff133302cfc42f9d6
Faizan Alam <faizanalam416@gmail.com>
2024-11-10 19:44:29 +0530

    DB change

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Connection error', err));
   } catch (err) {
-    console.error(err.message);
     process.exit(1); // Exit the process with failure
   }
 };
